434f6605df1d90ff0231da331112f454424dc645
Alay <alexeypekin13@gmail.com>
2022-08-09 22:56:19 +0300

    program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//  Написать программу,которая из имеющегося массива строк формирует массив из строк,
+//  длина которых меньше либо равна 3 символа.
+//  Первоначальный массив можно ввести с клавиатуры,либо задать на старте выполнения.
+
+string[] Array = { "Geek", "Brains", "you", "do" };
+
+void PrintArray()
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void SortLengthElements()
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+            Console.Write(Array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Заданный массив:");
+PrintArray();
+Console.WriteLine("Массив из строк, длина которых меньше либо равна 3 символа:");
+SortLengthElements();
